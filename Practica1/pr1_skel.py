279c580531b25331e54a655266ba69cda56ef2e8
Agusmoure <amoure@ucm.es>
2024-09-25 21:21:14 +0200

    Mejoras al codigo y segunda parte

--- Practica1/pr1_skel.py
+++ Practica1/pr1_skel.py
@@ -16,8 +16,6 @@ de manera directa o indirecta. Declaramos además que no hemos realizado de mane
 deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los
 resultados de los demás.
 """
-
-
 # Ejercicio 1
 a=[[1, 0, 2, 5],[0, 3, 3,5],[1, 2, 2, 5]]
 b=[[1, 0, 1],[0, 3, 2],[1, 2, 2]]
@@ -40,10 +38,14 @@ def dimension(matriz):
 
 
 def es_cuadrada(matriz):
+    """
+    Devuelve si una matriz es cuadrada o no
+    Si la matriz no es valida devuelve None
+    """
     if dimension(matriz)=="None":#Si la matriz esta malformada o es vacía entonces no es cuadrada
         return False
     return len(matriz)==len(matriz[0])
-    
+
 def es_simetrica(matriz):
     if not es_cuadrada(matriz): #Si una matriz no es cuadrada no puede ser simétrica
         return False 
@@ -54,18 +56,23 @@ def es_simetrica(matriz):
                 return False
     return True
 
-
+#TODO Aqui cambiaria la i para usar el enumerate en el bucle
 def multiplica_escalar(matriz, k):
-    if dimension(matriz)=="None":#Si la matriz esta mal formada o esta vacía entonces no se puede multiplicar
+    """
+    Genera una nueva matriz donde todos los elementos 
+    de la matriz pasada por argumento son multiplicados por k
+    """
+    #Si la matriz esta mal formada o esta vacía entonces no se puede multiplicar
+    if dimension(matriz)=="None":
         return "None"
-    newMat=[]
+    new_mat=[]
     i=0 #Marcador de la fila actual
     for fila in matriz:
-        newMat.append([])#Añadimos la fila vacía a la que se le van a añadir los valores
-        for n in fila:
-            newMat[i].append(n*k) #Añadimos cada valor
+        new_mat.append([])#Añadimos la fila vacía a la que se le van a añadir los valores
+        for elem in fila:
+            new_mat[i].append(elem*k) #Añadimos cada valor
         i=i+1 #actualizar marcador
-    return newMat
+    return new_mat
 
 
 def suma(matriz1, matriz2):
@@ -89,39 +96,79 @@ g = {"nodos": ["a", "b", "c", "d"],
                  }
      }
 
-
+from collections import Counter
+#TODO preguntar porque 8 returns son demasiados y como seria mejorable puesto que lo que hace es tener cada condicion de manera independiente
 def validar(grafo):
+    """
+    Validara un grafo siempre que se cumplan las condiciones
+    """
     claves = grafo.keys()
-    if (len(claves) != 2 or "nodos" not in grafo or "aristas" not in grafo):
+    if len(claves) != 2 or "nodos" not in grafo or "aristas" not in grafo:
         return False
 
-    if (len(grafo["nodos"]) == 0):
+    if len(grafo["nodos"]) == 0:
         return False
-    #Preguntar si podemos usar la biblioteca collections
-    # TODO Falta condicion de NO nodos repitidos
-
-    for nodo in g["nodos"]:
-        if(nodo not in g["aristas"]):#Si un nodo no aparece como nodo destino
+    #Comprobar que en grafo["nodos"] no haya mas de 1 vez el mismo nodo
+    #Si el valor más comun es 1 se sabe que todos los demas son 1
+    _, repeat=Counter(grafo["nodos"]).most_common(1)[0]
+    if repeat>1:
+        print("Nodo repetido en grafo[nodos]")
+        return False
+    #Todos los nodo en grafo["nodos"]debe estar en g["aristas"]
+    for nodo in grafo["nodos"]:
+        if nodo not in grafo["aristas"]:#Si un nodo no aparece como nodo origen
+            print("el nodo %s no aparece en grafo[aristas]",nodo)
             return False
 
-    for nodoO, nodosD in g["aristas"]:
-        if nodoO not in g["nodos"]:#Si el nodo origen no es valido
+    for nodo_o, nodos_d in grafo["aristas"].items():
+        if nodo_o not in grafo["nodos"]:#Si el nodo origen no es valido
+            print("Nodo origen no existe en los nodos proporcionados")
             return False
-        for nodoD in nodosD:
-            if(nodoD not in g["nodos"]):#Si algun nodo destino no es valido
+        _, repeat=Counter(nodos_d).most_common(1)[0]
+        if repeat>1:
+            print("Nodo repetido en nodos destino de %s",nodo_o)
+            return False
+        for nodo_d in nodos_d:
+            if nodo_d not in grafo["nodos"]:#Si algun nodo destino no es valido
+                print("Nodo destino no existe en los nodos proporcionados")
                 return False
     return True
 
+
 def grado_entrada(grafo, nodo):
-    if(not validar(grafo)or nodo not in grafo["nodos"]):
+    """
+    Determina el grado de entrada del nodo en el grafo
+    """
+    if not validar(grafo)or nodo not in grafo["nodos"]:
         return -1
     aux=0
-    for o,d in g["aristas"].items():
-        if nodo in d:
-            aux=aux+1 
+    for _,destiny in grafo["aristas"].items():
+        if nodo in destiny:
+            aux=aux+1
     return aux
 
 
 def distancia(grafo, nodo):
-    ...
-   
+    """
+    Determina la distancia a todos los nodos desde el nodo
+    """
+    visit=[]
+    dist={}
+    pending=[]
+    pending.append(nodo)
+    dist[nodo]=0
+    visit.append(nodo)
+    if not validar(grafo) or nodo not in grafo["nodos"]:
+        return "None"
+    while len(pending)>0:
+        current=pending[0]
+        pending.pop()
+        for elem in grafo["aristas"][current]:
+            if elem not in visit :
+                pending.append(elem)
+                visit.append(elem)
+                dist[elem]=dist[current]+1
+    for nodo_aux in grafo["nodos"]:
+        if nodo_aux not in dist:
+            dist[nodo_aux]=-1
+    return dist
