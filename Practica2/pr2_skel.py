54580fd29c6fdb6f702f98e99f88e6a8f9933ce8
MeraKy43 <papadial@ucm.es>
2024-09-26 15:38:17 +0200

    Practica2_Ej2

--- Practica2/pr2_skel.py
+++ Practica2/pr2_skel.py
@@ -3,8 +3,11 @@ TODO: rellenar
 
 Asignatura: GIW
 Práctica 2
-Grupo: XXXXXXX
-Autores: XXXXXX 
+Grupo: 08
+Autores: Autores: Carlos Rondon Arevalo
+         Pablo Padial Iniesta
+         David Llanes Martín
+         Agustín Moure Rodríguez 
 
 Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
 sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
@@ -15,16 +18,22 @@ resultados de los demás.
 """
 
 import csv
-from pprint import pprint
+from pprint import pp
+from collections import defaultdict
 ### Formato CSV
 def lee_fichero_accidentes(ruta):
-    with open("AccidentesBicicletas_2021.csv", 'r', newline='', encoding='utf8') as fich:
-     ejemploLector = csv.reader(fich)
-    for linea in ejemploLector:
-        print(f"Linea #{ejemploLector.line_num} {linea}")
+        with open(ruta, 'r', newline='',encoding='utf8') as fich:
+            ejemploLector = csv.DictReader(fich, delimiter=';')
+            for linea in ejemploLector:
+                 print(f"{linea}")
 
-def accidentes_por_distrito_tipo(datos):
-    ...
+def contar_accidentes(datos):
+    contador = defaultdict(int)
+    for accidente in datos:
+        distrito = accidente.get('distrito')
+        tipo_accidente = accidente.get('tipo_accidente')
+        contador[(distrito, tipo_accidente)] += 1
+    return dict(contador)
 
 def dias_mas_accidentes(datos):
     ...
