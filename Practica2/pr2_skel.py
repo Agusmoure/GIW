7f4380fb1c73d3392791159f64a8ae1e0ff9ce34
Agusmoure <amoure@ucm.es>
2024-09-28 23:59:20 +0200

    Parte Json
    A falta de dos TODO y revisar con pylint la parte de json esta(creo)

--- Practica2/pr2_skel.py
+++ Practica2/pr2_skel.py
@@ -43,14 +43,46 @@ def puntos_negros_distrito(datos, distrito, k):
 
 
 #### Formato JSON
+import json
+import pprint
+from collections import Counter
+from geopy.geocoders import Nominatim
+from geopy import distance
 def leer_monumentos(ruta):
-    ...
+    with open(ruta, 'r',newline='',encoding='utf8')as fich:
+        json_dict=json.load(fich)
+    return json_dict["@graph"]
+
 
 def codigos_postales(monumentos):
-    ...
+    postal_code_monuments= [monumento["address"]["postal-code"] for monumento in monumentos ]
+    return Counter(postal_code_monuments).most_common() 
 
 def busqueda_palabras_clave(monumentos, palabras):
-    ...
+    lista=[]
+    for monumento in monumentos:
+        words=True
+        for palabra in palabras:
+            desc_and_title=monumento["organization"]["organization-desc"]+" "+monumento["title"]
+            if palabra.lower() not in desc_and_title.lower() :
+                words=False
+        if(words):
+            lista.append((monumento["title"],monumento["address"]["district"]["@id"]))
+    return lista
+
 
 def busqueda_distancia(monumentos, direccion, distancia):
-    ...
+    geolocator = Nominatim(user_agent="GIW_pr2")
+    lista=[]
+    dir=geolocator.geocode(direccion,addressdetails=True)
+    for monumento in monumentos:
+        if "location" not in monumento:
+            #TODO Preguntar si los que no tienen location hay que sacar su direccion mediante calle y localizacion porque hay alguno que no tienen esas cosas tambien me parece
+            continue
+        lugar=(monumento["location"]["latitude"],monumento["location"]["longitude"])
+        distancia_al_monumento=distance.distance((dir.latitude,dir.longitude), lugar).km
+        if distancia_al_monumento<distancia:
+            lista.append((monumento["title"],monumento["id"],distancia_al_monumento))
+            #Sacado de la documentación de sort si estan a la misma distancia mantiene el orden de aparición
+            lista.sort(key=lambda monumento:monumento[2])
+    return lista
