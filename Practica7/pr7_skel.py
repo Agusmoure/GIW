1a9ce25f984dff886ac8848a373e1243d468a74f
Agusmoure <amoure@ucm.es>
2024-11-10 00:51:25 +0100

    P7 comentada
    faltan 6 cosas de pylint que no sé como arreglar y habría que preguntarle a Enrique

--- Practica7/pr7_skel.py
+++ Practica7/pr7_skel.py
@@ -14,7 +14,7 @@ deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perju
 resultados de los demás.
 """
 
-from flask import Flask, request, session, render_template
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -28,24 +28,30 @@ id_actual=0
 def comprueba_horario(horario):
     """
     Devuelve si el horario es correcto
-    y en caso de error cual es el fallo
+    y en caso de error cual es el fallo tambien
     """
+    #Tiene exactamente las claves necesarias
     if len(horario)!=3:
         return False,"Algun horario no tiene suficientes claves o tiene mas de estas"
+    #las claves son las necesarias
     if "dia" not in horario or "hora_inicio" not in horario or "hora_final" not in horario:
         return False, "Alguna de las claves del horario esta mal"
+    #el valor de las claves son correctas
     if not isinstance(horario["dia"],str) or isinstance(horario["hora_inicio"],bool) or not isinstance(horario["hora_inicio"],int) or isinstance(horario["hora_final"],bool) or not isinstance(horario["hora_final"],int):
         return False,"Algun tipo del horario es incorrecto"
     return True, ""
 def comprueba_asignatura(asignatura):
     """
     Devuelve si la asignatura es correcta
-    en caso de no serlo devuelve el error
+    en caso de no serlo devuelve el error tambien
     """
+    #Solo tiene 3 claves
     if len(asignatura)!=3:
         return False,"numero de claves incorrecto"
+    #Las claves son exactamente las necesarias
     if "nombre" not in asignatura or "numero_alumnos" not in asignatura or "horario" not in asignatura:
         return False,"alguna clave es erronea"
+    #los valores de las claves son correctos
     if not isinstance(asignatura["nombre"],str) or isinstance(asignatura["numero_alumnos"],bool) or not isinstance(asignatura["numero_alumnos"],int) or not isinstance(asignatura["horario"],list):
         return False,"tipos incorrectos"
     for fecha in asignatura["horario"]:
@@ -54,6 +60,10 @@ def comprueba_asignatura(asignatura):
             return False,mensaje
     return True, ""
 def get_asignatura_codigo(codigo):
+    """
+    Devuelve si ha encontrado la asignatura con el codigo asociado
+    en caso positivo tambien devuelve la asignatura
+    """
     found=False
     asig_encontrada={}
     for asignatura in asignaturas:
@@ -105,10 +115,12 @@ def get_global():
     Devuelve las asignaturas pedidas
     """
     params=request.args
+    #si no tiene parametros devuelve para evitar perder tiempo comparando
     if len(params)==0:
         response={"asignaturas":["/asignaturas/"+str(asignatura["id"])
                                     for asignatura in asignaturas]}
         return(response,200)
+    #argumentos validos
     validos=("page","per_page","alumnos_gte")
     #Confirmamos que todos los parametros sean validos
     for param in params:
@@ -130,9 +142,6 @@ def get_global():
         per_page=int(params["per_page"])
         start=per_page*(page-1)
         end=start+per_page
-        print(start)
-        print(end)
-        print("peticion")
         if start<=len(restantes):
             if end<len(restantes):
                 restantes=restantes[start:end]
@@ -141,8 +150,6 @@ def get_global():
         else:
             restantes=[]
     response={"asignaturas":["/asignaturas/"+str(asignatura["id"]) for asignatura in restantes]}
-    print("restantes:")
-    print(response)
     if len(asignaturas)==len(restantes):
         return(response,200)
     return(response,206)
@@ -150,6 +157,9 @@ def get_global():
 #Seccion de /asignaturas/X
 @app.route('/asignaturas/<int:numero>',methods=["DELETE"])
 def delete_asignatura(numero):
+    """
+    Elimina una asignatura
+    """
     found,asignatura=get_asignatura_codigo(numero)
     if found:
         asignaturas.remove(asignatura)
@@ -158,6 +168,9 @@ def delete_asignatura(numero):
 
 @app.route('/asignaturas/<int:numero>',methods=["GET"])
 def get_asignatura(numero):
+    """
+    Obtiene una asignatura
+    """
     found, asignatura=get_asignatura_codigo(numero)
     if found:
         return(asignatura,200)
@@ -165,6 +178,9 @@ def get_asignatura(numero):
 
 @app.route('/asignaturas/<int:numero>',methods=["PUT"])
 def put_asignatura(numero):
+    """
+    Cambia todos los datos de una asignatura
+    """
     found,asignatura=get_asignatura_codigo(numero)
     if not found:
         return("",404)
@@ -178,14 +194,17 @@ def put_asignatura(numero):
     return("",200)
 @app.route('/asignaturas/<int:numero>',methods=["PATCH"])
 def patch_asignatura(numero):
+    """
+    Actualiza uno de los datos de la asignatura
+    """
     found,asignatura=get_asignatura_codigo(numero)
     if not found:
         return("",404)
     validas=("nombre","horario","numero_alumnos")
-    print(len(request.get_json().keys()))
     if len(request.get_json().keys())!=1:
         return ("",400)
-    print("Pilla clave")
+    #keys no es una lista sino una dict_key si no no podemos acceder al
+    #valor mediante [0]
     clave=list(request.get_json().keys())[0]
     if clave not in validas:
         return("",400)
@@ -195,6 +214,9 @@ def patch_asignatura(numero):
 #seccion de /asignaturas/X/horario
 @app.route('/asignaturas/<int:numero>/horario',methods=["GET"])
 def get_horario(numero):
+    """
+    Devuelve el horario de una asignatura
+    """
     found,asignatura=get_asignatura_codigo(numero)
     if not found:
         return("",404)
@@ -204,11 +226,5 @@ if __name__ == '__main__':
     app.config['ENV'] = 'development'
     app.config['DEBUG'] = True
     app.config['TEST'] = True
-
-    # Imprescindible para usar sesiones
-    app.config['SECRET_KEY'] = 'giw_clave_secreta'
-
-    app.config['STATIC_FOLDER'] = 'static'
-    app.config['TEMPLATES_FOLDER'] = 'templates'
 
     app.run()
